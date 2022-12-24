2e1b23d83c9c0c6f21ff2eba4ff6a6a55efaa41d
Jaewook Choi <eari100@naver.com>
2022-12-24 18:16:50 +0900

    refactor: WebClient 를 이용한 소환사 정보 조회 API 리펙토링
    - 로직 Service Layer 로 분리

--- src/main/java/com/example/studywebclient/controller/SummonerApiController.java
+++ src/main/java/com/example/studywebclient/controller/SummonerApiController.java
@@ -2,12 +2,9 @@ package com.example.studywebclient.controller;
 
 import com.example.studywebclient.service.SummonerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
@@ -15,9 +12,6 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RestController
 public class SummonerApiController {
-
-    @Value("${riot.api.key}")
-    private String apiKey;
 
     private final SummonerService summonerService;
 
@@ -27,11 +21,7 @@ public class SummonerApiController {
     }
 
     @GetMapping("/v2/summoners/{summonerName}")
-    public Mono<Map> getSummonerV2(@PathVariable String summonerName) {
-        return WebClient.create(String.format("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%s", summonerName))
-                .get()
-                .header("X-Riot-Token", apiKey)
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<>() {});
+    public Mono getSummonerV2(@PathVariable String summonerName) {
+        return summonerService.getSummonerV2(summonerName);
     }
 }
--- src/main/java/com/example/studywebclient/service/SummonerService.java
+++ src/main/java/com/example/studywebclient/service/SummonerService.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
@@ -25,5 +27,13 @@ public class SummonerService {
 
         return restTemplate.exchange(String.format("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%s", summonerName),
                 HttpMethod.GET, new HttpEntity(httpHeaders), new ParameterizedTypeReference<Map>() {}).getBody();
+    }
+
+    public Mono getSummonerV2(String summonerName) {
+        return WebClient.create(String.format("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%s", summonerName))
+                .get()
+                .header("X-Riot-Token", apiKey)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<>() {});
     }
 }
