85ba436eb0be4b9110b977b2490a3c3714cd5a30
Jaewook Choi <eari100@naver.com>
2022-12-24 18:14:22 +0900

    refactor: 소환사 정보 조회 API 리펙토링
    - 로직 Service Layer 로 분리

--- src/main/java/com/example/studywebclient/controller/SummonerApiController.java
+++ src/main/java/com/example/studywebclient/controller/SummonerApiController.java
@@ -1,33 +1,29 @@
 package com.example.studywebclient.controller;
 
+import com.example.studywebclient.service.SummonerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
+@RequiredArgsConstructor
 @RestController
 public class SummonerApiController {
 
     @Value("${riot.api.key}")
-    String apiKey;
+    private String apiKey;
+
+    private final SummonerService summonerService;
 
     @GetMapping("/v1/summoners/{summonerName}")
     public Map<String, Object> getSummonerV1(@PathVariable String summonerName) {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.set("X-Riot-Token", apiKey);
-
-        return restTemplate.exchange(String.format("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%s", summonerName),
-                HttpMethod.GET, new HttpEntity(httpHeaders), new ParameterizedTypeReference<Map>() {}).getBody();
+        return summonerService.getSummonerV1(summonerName);
     }
 
     @GetMapping("/v2/summoners/{summonerName}")
---
+++ src/main/java/com/example/studywebclient/service/SummonerService.java
@@ -0,0 +1,29 @@
+package com.example.studywebclient.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Transactional
+@Service
+public class SummonerService {
+
+    @Value("${riot.api.key}")
+    private String apiKey;
+
+    public Map getSummonerV1(String summonerName) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("X-Riot-Token", apiKey);
+
+        return restTemplate.exchange(String.format("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%s", summonerName),
+                HttpMethod.GET, new HttpEntity(httpHeaders), new ParameterizedTypeReference<Map>() {}).getBody();
+    }
+}
